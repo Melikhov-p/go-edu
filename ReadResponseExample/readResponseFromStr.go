a303996b06d4a9b1424c13d0e9b05b7e24635816
Melikhov-p <melikhov.pavel@gmail.com>
2024-10-07 12:55:49 +0300

    feat(readResponseFromStr): exmaple function of reading json to Response struct from string

---
+++ ReadResponseExample/readResponseFromStr.go
@@ -0,0 +1,66 @@
+package ReadResponseExample
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+const rawResp = `
+{
+    "header": {
+        "code": 0,
+        "message": ""
+    },
+    "data": [{
+        "type": "user",
+        "id": 100,
+        "attributes": {
+            "email": "bob@yandex.ru",
+            "article_ids": [10, 11, 12]
+        }
+    }]
+}
+`
+
+type (
+	Response struct {
+		Header ResponseHeader `json:"header"`
+		Data   ResponseData   `json:"data"`
+	}
+
+	ResponseHeader struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	}
+
+	ResponseData []ResponseDataItem
+
+	ResponseDataItem struct {
+		Type  string                `json:"type"`
+		Id    int                   `json:"id"`
+		Attrs ResponseDataItemAttrs `json:"attributes"`
+	}
+
+	ResponseDataItemAttrs struct {
+		Email      string `json:"email"`
+		ArticleIDs []int  `json:"article_ids"`
+	}
+)
+
+func ReadResponse(rawData string) (Response, error) {
+	resp := Response{}
+	if err := json.Unmarshal([]byte(rawResp), &resp); err != nil {
+		return Response{}, fmt.Errorf("JSON unmarshal: %w", err)
+	}
+
+	return resp, nil
+}
+
+func main() {
+	resp, err := ReadResponse(rawResp)
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+	} else {
+		fmt.Printf("%+v\n", resp)
+	}
+}
